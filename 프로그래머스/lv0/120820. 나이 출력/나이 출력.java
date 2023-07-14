6f5b80504cabeaf9760c9cae20d09f435a55569a
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-14 09:20:40 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int age) {
+        
+        int answer = (2022 - age) + 1;     
+        
+      //  if(0 > age || age > 120)
+       //     return 0;
+        //else
+            return answer;       
+            
+
+       
+    }
+}

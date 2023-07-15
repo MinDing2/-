713c4cefb87b003c481f4c2687055b49e57cee4f
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-15 23:53:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+		    String str = in.next();
+		    char a = str.charAt(0);
+		 
+ 
+		    System.out.printf("%d", (int)a);
+	}
+}

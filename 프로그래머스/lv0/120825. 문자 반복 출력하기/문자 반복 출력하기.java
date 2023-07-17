11a2d3b520c3580b1258a3ffa153b674486f3b38
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-17 15:47:36 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.33 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -5,8 +5,12 @@ class Solution {
         //문자열 my_string과 정수 n 
         //my_string에 들어있는 각문자를 n만큼 반복한 문자
         //열을 return 하도록 solution 함수를 완성
-        String[] answer = new String[my_string.length() * n];
         int cnt = 0;
+        String anw ="";
+        String match = "[^a-z-A-Z]";
+        String[] answer = new String[my_string.length() * n];
+
+        
         
         for(int i = 0; i < my_string.length(); i++){
             for(int j = 0; j < n; j++){   
@@ -18,12 +22,8 @@ class Solution {
     
         String answerToString = Arrays.toString(answer);
         
-        
-        String anw ="";
-        String match = "[^a-zA-Z]";
+
         anw = answerToString.replaceAll(match,"");
-        
-     
         
         return anw;
     }

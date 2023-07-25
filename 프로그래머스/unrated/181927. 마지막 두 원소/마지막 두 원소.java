e587a9906bd04a789a2b55afb5c8a4db8f69ba65
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-25 10:00:59 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,9 +2,10 @@ class Solution {
     public int[] solution(int[] num_list) {
         
         int a = 0; 
-	
+        int[] answer = new int[num_list.length + 1];
+        
 	if(num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-		int[] answer = new int[num_list.length + 1];
+		
 	    a = num_list[num_list.length - 1] - num_list[num_list.length - 2];
 	    
 	    for(int i = 0; i < num_list.length; i++) {
@@ -15,12 +16,12 @@ class Solution {
 	  
 	}
 	else{
-		int[] answer = new int[num_list.length + 1];
 	    a = num_list[num_list.length - 1] * 2;
-	    
+
 	    for(int i = 0; i < num_list.length; i++) {
 	    	answer[i] = num_list[i]; 
 	    }
+        
 	    answer[num_list.length] = a;
         
     	return answer;      

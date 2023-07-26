1e683fc4705893a8093c1dd0315bec59c3a30a29
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-27 07:04:56 +0900

    [unrated] Title: 문자열 섞기, Time: 11.80 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,27 +1,11 @@
 class Solution {
     public String solution(String str1, String str2) {
-       		char[] a =new char[str1.length()];
-		char[] b =new char[str2.length()];
-		
-		for (int i = 0; i < b.length; i++) {
-			a[i]	= str1.charAt(i);
-			b[i]	= str2.charAt(i);
-		}
-		int numLength = str1.length() + str2.length();
 		
 		String answer = "";
-		
-		int aCnt = 0;
-		int bCnt = 0;
-		
-		for (int i = 0; i < numLength; i++) {
-			if(i % 2 == 0) {
-				answer += a[aCnt];
-                aCnt++;
-			}else {
-				answer += b[bCnt];
-                bCnt++;
-			}
+
+		for (int i = 0; i < str1.length(); i++) {
+			answer += str1.charAt(i);
+            answer += str2.charAt(i);
 		}
 		
 		return answer; 

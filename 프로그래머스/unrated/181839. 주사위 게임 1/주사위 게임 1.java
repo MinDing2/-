3d6db273bc67951d5fb6b108dee0c7fa6af0b4eb
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-26 09:16:07 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.Math;
+
+class Solution {
+    public int solution(int a, int b) {
+        double answer = 0;
+        if(a % 2 == 1 && b % 2 == 1) {
+		  answer = Math.pow(a, 2) + Math.pow(b, 2);
+		}else if(a % 2 == 1 || b % 2 == 1) {
+			answer = 2 * (a + b);
+		}else{
+			answer = a - b;
+		        if(answer < 0){
+				answer = -answer; 
+			    }
+		}
+        
+        return (int)answer;
+    }
+}

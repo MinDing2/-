ff6a0f435ffc0e33143719a753a8e331cded3199
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-28 17:13:24 +0900

    [unrated] Title: 접미사 배열, Time: 13.49 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -1,19 +1,6 @@
-import java.util.*;
 class Solution {
-    public  ArrayList<String> solution(String my_string) {
-        ArrayList<String> answer =new ArrayList<>();
-		String str ="";
-	
-		for (int i = 0; i < my_string.length(); i++) {
-			for (int j = i; j < my_string.length(); j++) {
-				str += my_string.charAt(j);
-			}
-			answer.add(str);
-			str ="";
-		} 
-        
-		Collections.sort(answer);
-        
+    public String[] solution(String my_string) {
+        String[] answer = {};
         return answer;
     }
 }

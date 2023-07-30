569cf2971dc6acd7e1759c9f4e60eec4d2691227
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-07-31 00:03:05 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,24 +1,27 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
+      
+	    int[] answer = new int[queries.length];
+	    int cnt = 0;
+	    int cnt2 = 0;
 
-        for (int i = 0; i < queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
-            int smallest = Integer.MAX_VALUE;
-
-            for (int j = s; j <= e; j++) {
-                if (arr[j] > k && arr[j] < smallest) {
-                    smallest = arr[j];
-                }
-            }
-
-            answer[i] = (smallest != Integer.MAX_VALUE) ? smallest : -1;
-        }
-
+	    
+	    for (int i = 0; i < queries.length; i++) {
+	        for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+	            if (arr[j] > queries[i][2]) {
+	                if (cnt2 == 0) {
+	                    answer[cnt] = arr[j];
+	                    cnt2 = 1;
+	                } else if (answer[cnt] > arr[j]) {
+	                    answer[cnt] = arr[j];
+	                }
+	            } else {
+	                answer[cnt] = -1;
+	            }
+	        }
+	        cnt2 = 0;
+	        cnt++;
+	    }
         return answer;
     }
 }

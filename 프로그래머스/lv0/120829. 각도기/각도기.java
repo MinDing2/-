c33951b9017ad4fc26e9f30479d54ed0358aa69c
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-03 13:50:18 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -2,15 +2,34 @@ class Solution {
     public int solution(int angle) {
         int answer = 0;  
         
-        if(angle > 0  && angle < 90)
-            return answer + 1;
-        else if(angle == 90)
-            return answer + 2;
-        else if(angle > 90  && angle < 180)
-            return answer + 3;
-        else if(angle == 180)
-            return answer + 4;
-           
-        return answer;
+        int num = 0; 
+    		
+        if(angle > 0  && angle < 90){
+     	    num = 1;
+        }else if(angle == 90){
+      	    num = 2;
+        }else if(angle > 90  && angle < 180){
+           	num = 3 ;
+        }else if(angle == 180){
+        	num = 4;
+        } 
+        
+           switch(num){
+           case 1:
+               answer = 1;
+               break;
+           case 2:
+               answer = 2;
+               break;
+           case 3:
+               answer = 3;
+               break;
+           case 4:
+               answer = 4;
+               break; 
+         } 
+        
+        return answer;  
+                   
     }
 }

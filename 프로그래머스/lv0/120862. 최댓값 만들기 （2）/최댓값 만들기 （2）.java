90568be8ba341874d8b97daf079a3d83e65b90cf
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-05 22:39:09 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] numbers) {
+        /*
+         * 정수 배열 numbers
+         * numbers의 원소 중 
+         * 두개를 곱해 만들 수 있는 최댓값
+         * return
+         *
+         */
+        
+        int answer = -1000000000;
+        
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = 0; j < numbers.length; j++){
+                if(answer < numbers[i] * numbers[j] && i != j) {
+                      answer = numbers[i] * numbers[j];
+                }   
+            }        
+        }
+        
+        return answer;
+    }
+}

071642bdf62a3e7720aafe946ef7b5975eb53b9b
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-13 04:38:12 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String str = "";
+        
+        for(int i = 0; i < array.length; i++){
+           str = String.valueOf(array[i]);
+           for(int j = 0; j < str.length(); j++){
+               if(str.charAt(j) == '7'){
+                   answer++;
+               }
+           }
+        }
+        return answer;
+    }
+}

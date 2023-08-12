a518bfb81c73a8171904e8f122015701149d285a
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-12 23:42:36 +0900

    [Bronze III] Title: 영수증, Time: 204 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class  Main {
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int total = sc.nextInt();
+			int[] price = new int[9];
+			int sum = 0;
+			for(int i = 0; i < 9; i++) {
+				price[i] = sc.nextInt();
+				sum += price[i];
+			}
+			sum = total - sum; 
+			System.out.println(sum);
+	}
+}

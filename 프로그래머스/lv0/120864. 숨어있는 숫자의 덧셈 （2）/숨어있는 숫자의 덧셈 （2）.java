4c0df5fc9789978529a8f8b827cd5f796503001e
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-12 11:02:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 10.37 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(String my_string) {
+        String sr = "";
+		int answer = 0;
+		int cnt = 0; 
+		
+		for (int i = 0; i < my_string.length(); i++) {
+			if(cnt != 0 && my_string.charAt(i) < 65) {
+				sr += " ";
+				sr += my_string.charAt(i);
+				cnt = 0; 
+			}else if(my_string.charAt(i) < 65 ) {
+				sr += my_string.charAt(i); 
+				cnt = 0;
+			}else{
+				cnt++;
+			}
+		}
+		
+		String[] str = sr.split(" "); 
+		
+		for (int i = 0; i < str.length; i++) {
+			if(str[i].equals("")) {
+				str[i] = "0";
+			}
+			if(1 <= Integer.parseInt(str[i]) && Integer.parseInt(str[i]) < 1000  && str[i] != null) {
+				answer += Integer.parseInt(str[i]);
+			}
+		}
+        return answer;
+    }
+}

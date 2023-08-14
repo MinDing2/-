64bfcb609ff3bd740a203df388f725e58462cd49
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-14 11:56:18 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.17 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] str = s.split(" ");
+
+        for(int i = 0; i < str.length; i++){ 
+            if(str[i].equals("Z")){
+              str[i] = "";
+              str[i - 1] = ""; 
+            }
+        }
+        
+        for(int i = 0; i < str.length; i++){ 
+            if(str[i] != ""){
+                answer += Integer.parseInt(str[i]);
+            }
+        }
+        
+        return answer;
+    }
+}

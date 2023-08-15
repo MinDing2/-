b04dd1c610ea9b34c4c664916b329934d5334a8c
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-15 18:59:10 +0900

    [unrated] Title: 배열 만들기 4, Time: 19.09 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+       
+        List<Integer> stk = new ArrayList<>();
+        int check = 0;
+            
+        for(int i = 0; i < arr.length; i++){
+    
+            if(stk.size() == 0){ 
+                stk.add(arr[i]);
+            }else if(stk.get(stk.size() - 1) < arr[i]){
+                stk.add(arr[i]);
+            }
+            
+             
+           for(int j = 0; j < stk.size(); j++){
+              if(stk.get(stk.size() - (j + 1)) >= arr[i]){ 
+                    stk.remove(stk.size() - (j + 1));
+                    j--;
+                }
+            }
+            if(stk.size() == 0){
+                stk.add(arr[i]);
+            }else if(stk.get(stk.size() - 1) < arr[i]){
+                stk.add(arr[i]);
+            }else{
+                stk.add(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[stk.size() ];
+        
+        for(int i = 0; i < stk.size(); i++){
+            answer[i] = stk.get(i);
+        }
+        
+        return answer;
+        
+    }
+}

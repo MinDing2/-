24b47166caca45e024296ef3e608e0560542dd0c
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-16 12:26:13 +0900

    [unrated] Title: 배열 만들기 6, Time: 18.18 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr){
+        List<Integer> stk = new ArrayList<>();                  
+        int[] answer = {};
+                                      
+        for(int i = 0; i < arr.length; i++){           
+            if(stk.size() == 0){
+                 stk.add(arr[i]);
+            }else if(stk.get(stk.size() - 1) == arr[i]){
+                stk.remove(stk.size() - 1);
+            }else if(stk.get(stk.size() - 1) != arr[i]){
+                stk.add(arr[i]);
+            }
+        }
+        
+        if(stk.size() == 0){
+            stk.add(-1);
+        }
+        
+        answer = new int[stk.size()];
+        for(int i = 0 ; i < stk.size(); i++){
+            answer[i] = stk.get(i);
+        }
+                                      
+        return answer;
+    }
+}

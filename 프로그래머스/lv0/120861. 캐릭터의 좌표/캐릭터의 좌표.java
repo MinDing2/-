1010dc7645a476acf82f69cedc68866ca93ad92f
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-08-19 12:39:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2] ;
+        Arrays.fill(answer, 0);
+        int width    = board[0] / 2;
+        int height   = board[1] / 2;
+
+        for(int i = 0; i < keyinput.length; i++){
+            if(keyinput[i].equals("left")  && answer[0] > -width){   
+                answer[0] -= 1;
+            }else if(keyinput[i].equals("right") && answer[0] < width){
+                answer[0] += 1;
+            }else if(keyinput[i].equals("up")    && answer[1] < height){
+                answer[1] += 1;
+            }else if(keyinput[i].equals("down")  && answer[1] > -height){
+                answer[1] -= 1;
+            }
+        }
+        return answer;
+    }
+}

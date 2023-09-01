899f9e7f5e113a1172267a8d7624991b36f5a2bf
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-09-01 17:50:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.09 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -3,35 +3,86 @@ import java.util.*;
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-       
-        //분모 분자 서로 곱해서 
-        //분모 분자를 맞춰줌
-        int denom   = denom1 * denom2;//16
-        int n1  = numer1 * denom2;//16
-        int n2  = numer2 * denom1;//16
-        int nSum = n1 + n2;//32
-        int denomCopy = denom;
-        int nSumCopy  = nSum;
+        //분자
+//         numer1
+//         numer2
         
-        if(denom >= nSum){//16 32
-            for(int i = 2; i <= nSum; i++){
-                if(denom % i == 0 && nSum % i == 0){
-                    denomCopy = denom / i;
-                    nSumCopy  = nSum / i;
-                } 
-            }
-        }else if(denom < nSum){//16 32 작동 
-            for(int i = 2; i <= denom; i++){// 2부터 16 까지  
-                if(denom % i == 0 && nSum % i == 0){//16 % i 가 0 32 % i 가  0이면 
-                    denomCopy = denom / i;// 16 / 2 = 8
-                    nSumCopy  = nSum / i;//  32 / 2 = 16 
-                }
-            }
+//         //분모
+//         denom1  denom2
+        
+        //답을 기약 분수로 나타내야댐 
+        
+        //일단 분모를 맞춰주고 
+        int sum;
+        int dm1  = denom1;
+        int dm2  = denom2; 
+        int num1 = numer1;
+        int num2 = numer2; 
+        int nSum = 0;
+        int dSum = 0;
+        boolean tr = true;
+        List<Integer> dv = new ArrayList<>(); 
+        
+
+        int cnt = 2;
+        while(true){
+             if(denom1 < denom2){
+                 dm1  = denom1 * cnt;
+                 num1 = numer1 * cnt; 
+                 cnt++; 
+             }else if(denom1 > denom2){
+                 dm2  = denom2 * cnt;
+                 num2 = numer2 * cnt; 
+                 cnt++;
+             }else{
+                 nSum = num1 + num2; 
+                 dSum = dm1 + dm2;     
+                 
+                 //기약분수 확인 되면 break
+                 
+                 if(nSum > dSum){
+                     for(int i = 2; i <= dSum; i++){
+                         if(dSum % i == 0){
+                            dv.add(i);
+                         }
+                     }
+                     for(int j = 0; j < dv.size(); j++){
+                          if(nSum % dv.get(j) == 0){
+                              tr = false;// 기약분수 x 
+                          }
+                     }
+                     if(tr){
+                         answer[0] = nSum;
+                         answer[1] = dSum;
+                         break;
+                     }else{
+                         continue; 
+                     }
+                     
+                 }else if(nSum < dSum){
+                       for(int i = 2; i <= nSum; i++){
+                         if(dSum % i == 0){
+                            dv.add(i);
+                         }
+                       }  
+                     for(int j = 0; j < dv.size(); j++){
+                          if(dSum % dv.get(j) == 0){
+                              tr = false;// 기약분수 x 
+                          }
+                     }
+                     if(tr){
+                         answer[0] = nSum;
+                         answer[1] = dSum;
+                         break;
+                     }else{
+                         continue; 
+                     }
+                 }else{
+                     answer[0] = 1;
+                     answer[1] = 1;
+                 }
+             }
         }
-        //나누는 수 i가 분모보다 작을 경우에도 약분이 가능하다면 약분해줄 수 있게 
-        answer[0] =  nSumCopy;
-        answer[1] =  denomCopy;
-        
         return answer;
     }
 }

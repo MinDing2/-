d59ae2657cd06f1d74836e91f00f9c042cf1aef9
MinDing2 <60898438+MinDing2@users.noreply.github.com>
2023-10-27 12:50:29 +0900

    [level unrated] Title: 안전지대, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120866. 안전지대/안전지대.java
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.java
@@ -1,60 +1,60 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] board) {
-        int answer = 0, a = board.length, b = board[0].length, cnt = 0;
-        int[][] boardCopy = new int[a][b];
+        int cnt = 0, size = board.length;
         
-        for(int i = 0; i < a; i++){
-            for(int j = 0; j < b; j++){
+        int[][] boardCopy = new int[board.length][board[0].length];
+        
+        //0으로 초기화
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
                 boardCopy[i][j] = 0; 
             }
         }
         
-        for(int i = 0; i < a; i++){
-            for(int j = 0; j < b; j++){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
                if(board[i][j] == 1){
-            //i
+            //상하좌우 1
                         boardCopy[i][j] = 1;   
                         if(j - 1 >= 0){
                             boardCopy[i][j - 1] = 1;
                         }
-                        if(j + 1 < b){
+                        if(j + 1 < size){
                             boardCopy[i][j + 1] = 1;
                         } 
-            //i - 1
+            //왼쪽 대각선 1
                         if(i - 1 >= 0 && j - 1 >= 0){
                             boardCopy[i - 1][j - 1] = 1;
                         } 
-                        if (i - 1 >= 0 && j + 1 < b){
+                        if (i - 1 >= 0 && j + 1 < size){
                             boardCopy[i - 1][j + 1] = 1;
                         }
                         if(i - 1 >= 0){
                             boardCopy[i - 1][j] = 1;  
                         }
-           // i + 1 
-                        if(i + 1 < a && j - 1 >= 0){
+           // 오른쪽 대각선 1
+                        if(i + 1 < size && j - 1 >= 0){
                             boardCopy[i + 1][j - 1] = 1;
                         } 
-                        if (i + 1 < a && j + 1 < b){
+                        if (i + 1 < size && j + 1 < size){
                             boardCopy[i + 1][j + 1] = 1;
                         }
-                        if(i + 1 < a){
+                        if(i + 1 < size){
                             boardCopy[i + 1][j] = 1;  
                         }
                 }   
             }
         }
         
-        for(int i = 0; i < a; i++){
-            for(int j = 0; j < b; j++){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
                 if(boardCopy[i][j] == 0){
                     cnt++;
                 }
             }
         }
         
-        answer = cnt; 
-    
-        return answer;
+        return cnt;
     }
 }
